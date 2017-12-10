e249be91f22d3f16d9a2b1c8e59a46247a8fe0b4
Christian Gunderman <gundermanc@gmail.com>
2017-12-10 14:47:23 -0800

    Added windows build scripts.

---
+++ BuildSelfTestWindows.cmd
@@ -0,0 +1,4 @@
+:: Cross Platform Tools for C/C++ Extensible Applications
+:: By: Christian Gunderman
+
+powershell.exe -ExecutionPolicy Bypass .\BuildSelfTestWindows.ps1
---
+++ BuildSelfTestWindows.ps1
@@ -0,0 +1,33 @@
+$cmakeCmd = "cmake.exe"
+
+if (!(Get-Command $cmakeCmd -ErrorAction SilentlyContinue)) 
+{ 
+   $cmakeCmd = "C:\Program Files\CMake\bin\cmake.exe"
+}
+
+if (!(Get-Command $cmakeCmd -ErrorAction SilentlyContinue)) 
+{
+    Write-Host -ForegroundColor Red "Cannot find CMAKE.exe"
+    Exit
+}
+
+if (!(Get-Command "msbuild" -ErrorAction SilentlyContinue)) 
+{
+    Write-Host -ForegroundColor Red "Cannot find MSBuild.exe"
+    Write-Host "Make sure that you are running from within a Visual Studio Developer Command prompt"
+    Exit
+}
+
+if (!(Get-Command "git" -ErrorAction SilentlyContinue)) 
+{
+    Write-Host -ForegroundColor Red "Cannot find Git.exe"
+    Write-Host "Make sure that Git for Windows is installed and on the path"
+    Exit
+}
+
+git submodule init
+git submodule update
+
+cd StABIlize
+& $cmakeCmd .
+msbuild StABIlize.sln
